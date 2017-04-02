f498d41ba2fa02a2fd445504e241b4d028a79bcf
qlin1 <qlin1@andrew.cmu.edu>
2017-04-02 14:49:13 -0400

    create patient model

--- capstone/capstoneapp/models.py
+++ capstone/capstoneapp/models.py
@@ -3,3 +3,9 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    patient_id = models.CharField(max_length=20)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
