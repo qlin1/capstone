ac4c5de62992b79ab2e45daf2a7fcf442d8caaac
Yazhou(Sophia) Xun <yxun@andrew.cmu.edu>
2017-04-02 14:14:11 -0400

    Add the API Request into view.py

--- capstone/capstoneapp/views.py
+++ capstone/capstoneapp/views.py
@@ -1,8 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
+    import urllib2
+    # If you are using Python 3+, import urllib instead of urllib2
+
+    import json
+
+
+    data =  {
+
+            "Inputs": {
+
+                    "input1":
+                    {
+                        "ColumnNames": ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Outcome"],
+                        "Values": [ [ "1", "0", "0", "0", "0", "0", "0", "0", "0" ], [ "0", "0", "0", "0", "0", "0", "0", "0", "0" ], ]
+                    },        },
+                "GlobalParameters": {
+    }
+        }
+
+    body = str.encode(json.dumps(data))
+
+    url = 'https://ussouthcentral.services.azureml.net/workspaces/b1f7d78580dd4c58a07358ee0bc4c192/services/37e6cf9820224c93837449ff8c97a908/execute?api-version=2.0&details=true'
+    api_key = 'gXF+v1g+42IGGOQdtKPvkj97MviDpjwTEZbhhPdX3J+hfqWIdNLIJPYTW+5akEDcOpoNju2PXg7FJJImHqH3kQ==' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+
+    req = urllib2.Request(url, body, headers)
+
+    try:
+        response = urllib2.urlopen(req)
+
+        # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+        # req = urllib.request.Request(url, body, headers)
+        # response = urllib.request.urlopen(req)
+
+        result = response.read()
+        print(result)
+    except urllib2.HTTPError, error:
+        print("The request failed with status code: " + str(error.code))
+
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
+
+        print(json.loads(error.read()))
 	return render(request, 'tool.html')
 
 def result(request):
