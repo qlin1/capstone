61865ed7d09e9a2e1b3a0801a9bc1d65ff818260
qlin1 <qlin1@andrew.cmu.edu>
2017-04-20 03:33:26 -0400

    Fulfilled report storing and page redirecting

--- capstone/capstoneapp/views.py
+++ capstone/capstoneapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from models import *
 from forms import *
 from django.core.urlresolvers import reverse
+from datetime import datetime
 
 import urllib2
 # If you are using Python 3+, import urllib instead of urllib2
@@ -14,116 +15,179 @@ def index(request):
     return render(request, 'tool.html', context)
 
 
-def result(request):
+def result(request, id):
+    patient_id = id
+    print("patient_id"+patient_id)
     context = {}
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = PatientForm(request.POST)
-        submit = request.POST.get('calculate',None)
     else:
         form = PatientForm()
-        submit = request.GET.get('calculate',None)
 
-    if submit:
-        patient_id = request.POST.get('patientId')
-        patient = Patient.objects.get(patient_id=patient_id)
-        print(patient.first_name)
-        print(patient.last_name)
-        patient_measurement = Measurement.objects.get(patient=patient)
-        print(patient_measurement.age)
-        print(patient_measurement.height)
+    patient = Patient.objects.get(patient_id=patient_id)
+    print(patient.first_name)
+    print(patient.last_name)
 
-        # If you are using Python 3+, import urllib instead of urllib2
-        Pregnancies=patient_measurement.pregnancies
-        Glucose=patient_measurement.glucose
-        BloodPressure=patient_measurement.blood_pressure
-        SkinThickness=patient_measurement.skin_thickness
-        Insulin=patient_measurement.insulin
-        BMI=patient_measurement.bmi
-        DiabetesPedigreeFunction=patient_measurement.diabetes_predigree_function
-        Age=patient_measurement.age
-        Outcome=1
-        DoctorComments=""
+    patient_measurement = Measurement.objects.get(patient=patient)
+    print(patient_measurement.age)
+    print(patient_measurement.height)
 
-        data =  {
-            "Inputs": {
-                "input1":{
-                    "ColumnNames": ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Outcome"],
-                    "Values": [ [ Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age, Outcome]]
-                },
+    report = Report.objects.filter(patient=patient)
+    count = report.count()
+    r_num = "%05d" % (count+1)
+    print("report num:")
+    print(count)
+    print(r_num)
+    report_id = "P"+patient_id+"587R"+r_num
+    print("report id:")
+    print(report_id)
+    r_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    print("report date:")
+    print(r_date)
+
+    # If you are using Python 3+, import urllib instead of urllib2
+    Pregnancies=patient_measurement.pregnancies
+    Glucose=patient_measurement.glucose
+    BloodPressure=patient_measurement.blood_pressure
+    SkinThickness=patient_measurement.skin_thickness
+    Insulin=patient_measurement.insulin
+    BMI=patient_measurement.bmi
+    DiabetesPedigreeFunction=patient_measurement.diabetes_predigree_function
+    Age=patient_measurement.age
+    Outcome=1
+
+    measurement = "Pregnancies "+str(Pregnancies)+"Glucose "+str(Glucose)+"BloodPressure "+str(BloodPressure)+"SkinThickness "+str(SkinThickness)+"Insulin "+str(Insulin)+"BMI "+str(BMI)+"DiabetesPedigreeFunction "+str(DiabetesPedigreeFunction)+"Age "+str(Age)
+    DoctorComments=""
+
+    data =  {
+        "Inputs": {
+            "input1":{
+                "ColumnNames": ["Pregnancies", "Glucose", "BloodPressure", "SkinThickness", "Insulin", "BMI", "DiabetesPedigreeFunction", "Age", "Outcome"],
+                "Values": [ [ Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age, Outcome]]
             },
-            "GlobalParameters": {
-            }
+        },
+        "GlobalParameters": {
         }
+    }
 
-        body = str.encode(json.dumps(data))
-        url = 'https://ussouthcentral.services.azureml.net/workspaces/b1f7d78580dd4c58a07358ee0bc4c192/services/37e6cf9820224c93837449ff8c97a908/execute?api-version=2.0&details=true'
-        api_key = 'gXF+v1g+42IGGOQdtKPvkj97MviDpjwTEZbhhPdX3J+hfqWIdNLIJPYTW+5akEDcOpoNju2PXg7FJJImHqH3kQ==' # Replace this with the API key for the web service
-        headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
+    body = str.encode(json.dumps(data))
+    url = 'https://ussouthcentral.services.azureml.net/workspaces/b1f7d78580dd4c58a07358ee0bc4c192/services/37e6cf9820224c93837449ff8c97a908/execute?api-version=2.0&details=true'
+    api_key = 'gXF+v1g+42IGGOQdtKPvkj97MviDpjwTEZbhhPdX3J+hfqWIdNLIJPYTW+5akEDcOpoNju2PXg7FJJImHqH3kQ==' # Replace this with the API key for the web service
+    headers = {'Content-Type':'application/json', 'Authorization':('Bearer '+ api_key)}
 
-        req = urllib2.Request(url, body, headers)
-        try:
-            response = urllib2.urlopen(req)
+    req = urllib2.Request(url, body, headers)
+    try:
+        response = urllib2.urlopen(req)
 
-            # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
-            # req = urllib.request.Request(url, body, headers)
-            # response = urllib.request.urlopen(req)
+        # If you are using Python 3+, replace urllib2 with urllib.request in the above code:
+        # req = urllib.request.Request(url, body, headers)
+        # response = urllib.request.urlopen(req)
 
-            result = response.read()
-            valuesResult=result.split("\"Values\":[[")[1]
-            valuesList=valuesResult.split(",")
-            size=len(valuesList)
-            finalResultString=valuesList[size-1]
-            finalResult=finalResultString[3]+finalResultString[4]+"."+finalResultString[5]+finalResultString[6]
-            print(result)
-            print(finalResult)
-        except urllib2.HTTPError, error:
-            print("The request failed with status code: " + str(error.code))
+        result = response.read()
+        valuesResult=result.split("\"Values\":[[")[1]
+        valuesList=valuesResult.split(",")
+        size=len(valuesList)
+        finalResultString=valuesList[size-1]
+        finalResult=finalResultString[3]+finalResultString[4]+"."+finalResultString[5]+finalResultString[6]
+        print(result)
+        print(finalResult)
+    except urllib2.HTTPError, error:
+        print("The request failed with status code: " + str(error.code))
 
-            # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
-            print(error.info())
+        # Print the headers - they include the requert ID and the timestamp, which are useful for debugging the failure
+        print(error.info())
 
-            print(json.loads(error.read()))
+        print(json.loads(error.read()))
 
-        # report = Report.objects.get(id=id)
+    # report = Report.objects.get(id=id)
 
-        if request.method == 'POST':
-            # create a form instance and populate it with data from the request:
-            form = CommentForm(request.POST)
-        else:
-            form = CommentForm()
-
-        context["patient_id"]=patient_id
-        context["Pregnancies"]=Pregnancies
-        context["Glucose"]=Glucose
-        context["BloodPressure"]=BloodPressure
-        context["SkinThickness"]=SkinThickness
-        context["Insulin"]=Insulin
-        context["BMI"]=BMI
-        context["DiabetesPedigreeFunction"]=DiabetesPedigreeFunction
-        context["Age"]=Age
-        context["Outcome"]=Outcome
-        context["DoctorComments"]=DoctorComments
-        context["Possibility"]=finalResult
-        r = Report(patient=patient, report_id="D100058700014",date="2017-04-20 20:20:52",measurement="Pregnancies 100 Glucose 100 BloodPressure 100 SkinThickness 100",prediction="100%",suggestion="Giving Metformin oral or Humulin r injection",comments="")
-        r.save()
-        return render(request, 'd_result.html', context)
-
+    if request.method == 'POST':
+         # create a form instance and populate it with data from the request:
+        form = CommentForm(request.POST)
     else:
-        records = Report.objects.all()
-        context["records"]=records
-        return render(request, 'd_dashboard.html', context)
+        form = CommentForm()
 
-def dashboard(request):
+    context["patient_id"]=patient_id
+    context["Pregnancies"]=Pregnancies
+    context["Glucose"]=Glucose
+    context["BloodPressure"]=BloodPressure
+    context["SkinThickness"]=SkinThickness
+    context["Insulin"]=Insulin
+    context["BMI"]=BMI
+    context["DiabetesPedigreeFunction"]=DiabetesPedigreeFunction
+    context["Age"]=Age
+    context["Outcome"]=Outcome
+    context["DoctorComments"]=DoctorComments
+    context["Possibility"]=finalResult
+    prediction=finalResult
+    suggestion="Giving Metformin oral or Humulin r injection"
+
+    report = Report(patient=patient, report_id=report_id,date=r_date, measurement=measurement, prediction=prediction, suggestion=suggestion, comments=DoctorComments)
+    report.save()
+
+
+    # report = Report.objects.get(id=id)
+    # print(report.count())
+    # report_id = "P"+patient_id
+    # r = Report(patient=patient, report_id="D100058700014",date="2017-04-20 20:20:52",measurement="Pregnancies 100 Glucose 100 BloodPressure 100 SkinThickness 100",prediction="100%",suggestion="Giving Metformin oral or Humulin r injection",comments="")
+    # r.save()
+    return render(request, 'd_result.html', context)
+
+    # records = Report.objects.all()
+    # context["records"]=records
+    # return render(request, 'd_dashboard.html', context)
+
+def dashboard(request, id):
     context={}
     # r=Report(report_id="D100058700010",date="2017-03-28 20:20:52",measurement="Pregnancies 15 Glucose 194 BloodPressure 72 SkinThickness 35",prediction="89%",suggestion="Giving Metformin oral or Humulin r injection",comments="")
     # r.save()
-    records = Report.objects.all()
-    print("@@@"+records[1])
+    patient = Patient.objects.get(patient_id=id)
+    records = Report.objects.filter(patient=patient)
     context["records"]=records
     return render(request, 'd_dashboard.html',context)
+
 def tool(request):
-    return render(request,'tool.html')
+    content={}
+
+    # Report.objects.all().delete()
+    # Measurement.objects.all().delete()
+    # test=Measurement.objects.all().count()
+    # print("delete m")
+    # print(test)
+
+    # p=Patient.objects.get(patient_id="1000")
+    # m = Measurement(patient=p, age="50", height="178", weight="50", pregnancies="15", glucose="194", insulin="0", blood_pressure="72", skin_thickness="35", bmi="33.6", diabetes_predigree_function="0.627", heartbeat="87")
+    # m.save()
+    # p2=Patient.objects.get(patient_id="1001")
+    # m = Measurement(patient=p2, age="31", height="145", weight="70", pregnancies="0", glucose="138", insulin="0", blood_pressure="66", skin_thickness="29", bmi="26.6", diabetes_predigree_function="0.351", heartbeat="90")
+    # m.save()
+    # test1=Measurement.objects.all().count()
+    # print("add m")
+    # print(test1)
+
+
+    patient_id = request.POST.get('patient_id')
+    # patient = Patient.objects.get(patient_id=patient_id)
+    # report = Report.objects.filter(patient=patient)
+    # count = report.count()
+    # r_num = "%05d" % (count+1)
+    # print("report num:")
+    # print(count)
+    # print(r_num)
+    # report_id = "P"+patient_id+"587R"+r_num
+    # print("report id:")
+    # print(report_id)
+    # r_date = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    # print("report date:")
+    # print(r_date)
+    #
+    # r = Report(patient=patient, report_id=report_id,date=r_date)
+    # r.save()
+
+    content['patient_id'] = patient_id
+    print("patientid"+content['patient_id'])
+    return render(request,'tool.html', content)
 
 def role(request):
     context={}
